a6464a073972d710c22d54601edcef852ce87c51
Barend <barend.poot@gmail.com>
2021-10-15 23:30:28 +0200

    kado en stonk en textures toegevoegd

--- annebirthday_godot/scripts/main.gd
+++ annebirthday_godot/scripts/main.gd
@@ -12,18 +12,18 @@ func _ready():
 	var grass_arr = [grass_2_scene, grass_1_scene, grass_1_scene, grass_1_scene]
 	
 	# spawn stars in the sky
-	for i in range(0, star_amount):
+	for _index in range(0, star_amount):
 		var star = star_scene.instance()
 		star.translation = Vector3(normal(0.0, 70.0), rand_range(60, 80), normal(0.0, 70.0))
 		add_child(star)
 	
 	# spawn grass on ground
-	for i in range(0, grass_amount):
+	for _index in range(0, grass_amount):
 		grass_arr.shuffle()
 		var grass = grass_arr[0].instance()
 		# add some randomness on the scale and rotation
 		var scale_factor = rng.randf_range(.1, .5)
-		var rotation = grass.rotate_y(rng.randf_range(0, 2*PI))
+		grass.rotate_y(rng.randf_range(0, 2*PI))
 		grass.scale = Vector3(scale_factor, scale_factor, scale_factor)
 		grass.translation = Vector3(normal(-5, 6.0), -.1, normal(-12, 6.0))		
 		add_child(grass)
