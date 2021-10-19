f8908463bb010a282da22ab10d65b5a076e8cebd
Barend <barend.poot@gmail.com>
2021-10-19 13:09:13 +0200

    added radio

--- annebirthday_godot/text_spot.gd
+++ annebirthday_godot/text_spot.gd
@@ -2,11 +2,14 @@ extends SpotLight
 
 var light_up = false
 
+func _ready():
+	light_energy = 0
+
 func _physics_process(delta):
 	if Input.is_action_just_pressed("open"):
 		light_up = true
 	
 	if light_up:
-		light_energy += .01
+		light_energy += .001
 		if light_energy > 10:
 			light_up = false
